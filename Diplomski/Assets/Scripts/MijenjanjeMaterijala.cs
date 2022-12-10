00314c283eeaecb2a82bd2d9b54fdb2b217856c5
SnezanaDespotovic <90354474+SnezanaDespotovic@users.noreply.github.com>
2022-12-10 19:37:42 +0100

    3D Unity

---
+++ Diplomski/Assets/Scripts/MijenjanjeMaterijala.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MijenjanjeMaterijala : MonoBehaviour
+{  
+    public Material [] material;
+    public int x;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x=0;
+        rend = GetComponent<Renderer>();
+        rend.enabled=true;
+        rend.sharedMaterial = material[x];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.sharedMaterial = material[x];
+    }
+    public void NextMaterial()
+    {
+        if(x<2)
+        {
+            x++;
+        }
+        else
+        {
+            x=0;
+        }
+    }
+}
